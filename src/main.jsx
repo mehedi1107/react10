1504d8727a0642efe4729bbe50bdfa6956dea489
mehedi1107 <mehedihasan2080bjg@gmail.com>
2025-01-29 15:18:38 -0800

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, HashRouter, RouterProvider } from "react-router-dom";
 import Home from "./component/Home/Home.jsx";
 import About from "./component/About/About.jsx";
 import Portfolio from "./component/Portfolio/Portfolio.jsx";
@@ -10,28 +10,28 @@ import Contact from "./component/Contact/Contact.jsx";
 import Skill from "./component/Skill/Skill.jsx";
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App></App>,
+    path: '/',
+    element: <App></App>,
     children: [
       {
-        path:'/home',
-        element:<Home></Home>
+        path: '/home',
+        element: <Home></Home>
       },
       {
-        path:'/about',
-        element:<About></About>
+        path: '/about',
+        element: <About></About>
       },
       {
-        path:'/portfolio',
-        element:<Portfolio></Portfolio>
+        path: '/portfolio',
+        element: <Portfolio></Portfolio>
       },
       {
-        path:'/skills',
-        element:<Skill></Skill>
+        path: '/skills',
+        element: <Skill></Skill>
       },
       {
-        path:'/contact',
-        element:<Contact></Contact>
+        path: '/contact',
+        element: <Contact></Contact>
       }
 
     ]
@@ -42,7 +42,10 @@ const router = createBrowserRouter([
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <HashRouter>
     <RouterProvider router={router} ></RouterProvider>
-  </StrictMode>,
+
+
+
+  </HashRouter>,
 )
